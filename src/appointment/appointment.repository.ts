324c36e1cec8043b64b4324692374a7739f8ac51
Moses Nkrumah <mosessccss@gmail.com>
2020-08-18 09:06:30 +0000

    querybuilder

--- src/appointment/appointment.repository.ts
+++ src/appointment/appointment.repository.ts
@@ -1,5 +1,14 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getConnection } from 'typeorm';
 import { AppointmentEntity } from './appointment.entity';
 
 @EntityRepository(AppointmentEntity)
-export class AppointmentRepository extends Repository<AppointmentEntity> {}
+export class AppointmentRepository extends Repository<AppointmentEntity> {
+  /**
+   * Gets all due appointments
+   */
+  public async getDueAppointments() {
+    const appointment = getConnection()
+      .createQueryBuilder()
+      .where();
+  }
+}
