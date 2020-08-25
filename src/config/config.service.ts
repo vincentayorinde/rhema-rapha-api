3cdca87deae8cf43480f88615eef3902e8f0c7fc
Moses Nkrumah <mosessccss@gmail.com>
2020-08-25 23:38:38 +0000

    typorm

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -12,29 +12,18 @@ export class ConfigService {
 
   constructor(filePath: string) {
     const config = dotenv.parse(fs.readFileSync(filePath));
-    this.envConfig = this.validateInput(config);
+    if (process.env.NODE_ENV === 'development') {
+      this.envConfig = this.validateInput(config);
+    }
   }
 
   public getTypeORMConfig(): TypeOrmModuleOptions {
     const baseDir = path.join(__dirname, '../');
     const entitiesPath = `${baseDir}${this.envConfig.TYPEORM_ENTITIES}`;
     const migrationPath = `${baseDir}${this.envConfig.TYPEORM_MIGRATIONS}`;
-    const type: any = this.envConfig.TYPEORM_CONNECTION;
-
+    // const type: any = this.envConfig.TYPEORM_CONNECTION;
     return {
-      type,
-
-      host: this.envConfig.TYPEORM_HOST,
-
-      username: this.envConfig.TYPEORM_USERNAME,
-
-      password: this.envConfig.TYPEORM_PASSWORD,
-
       database: this.envConfig.TYPEORM_DATABASE,
-
-      port: Number.parseInt(this.envConfig.TYPEORM_PORT, 10),
-
-      logging: false,
 
       entities: [entitiesPath],
 
@@ -47,6 +36,33 @@ export class ConfigService {
         entitiesDir: 'src/db/entities',
       },
     };
+
+    // return {
+    //   type,
+
+    //   host: this.envConfig.TYPEORM_HOST,
+
+    //   username: this.envConfig.TYPEORM_USERNAME,
+
+    //   password: this.envConfig.TYPEORM_PASSWORD,
+
+    //   database: this.envConfig.TYPEORM_DATABASE,
+
+    //   port: Number.parseInt(this.envConfig.TYPEORM_PORT, 10),
+
+    //   logging: false,
+
+    //   entities: [entitiesPath],
+
+    //   migrations: [migrationPath],
+    //   migrationsRun: this.envConfig.TYPEORM_MIGRATIONS_RUN === 'true',
+
+    //   cli: {
+    //     migrationsDir: 'src/db/migrations',
+
+    //     entitiesDir: 'src/db/entities',
+    //   },
+    // };
   }
 
   private validateInput(envConfig: IEnvConfigInterface): any {
